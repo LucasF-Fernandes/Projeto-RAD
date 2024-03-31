845e950e3d59859a9577bf17b9191b25d6e9a7d1
LucasF-Fernandes <162646208+LucasF-Fernandes@users.noreply.github.com>
2024-03-31 20:00:42 -0300

    Update interface grafica.py

--- interface grafica.py
+++ interface grafica.py
@@ -1,64 +1,76 @@
-import tkinter as tk
-import sqlite3 as conn
+import tkinter as tk #usando para criar a interface grafica
+import sqlite3 as conn #usando para usar o SQLite
+from tkinter import messagebox
 
-def dados_clientes():
+def dados_clientes(): #função para pegar os dados do cliente
     cpf = cpf_entry.get()
-    nome = nome_cliente_entry.get()
+    nome = nome_cliente_agenda_entry.get()
     nascimento = nascimento_entry.get()
     contato = contato_entry.get()
-    cursor.execute('''INSERT INTO Cliente (cpf, nome, nascimento, contato) VALUES (?, ?, ?, ?);''', (cpf, nome, nascimento, contato))
-    conn.commit()
-    display_clientes()
+    try:
+        cursor.execute('''INSERT INTO Cliente (cpf, nome, nascimento, contato) VALUES (?, ?, ?, ?);''', (cpf, nome, nascimento, contato))
+        conn.commit()
+        display_clientes()
+    except conn.DatabaseError as err:
+        messagebox.showerror('Erro', f'Erro ao inserir cliente: {err}')
 
-def display_clientes():
+def display_clientes(): #função para mostrar os dados já incluso na tabela clientes
     cursor.execute('''SELECT * FROM Cliente''')
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-def dados_barbeiros():
-    id = id_barbeiro_entry.get()
-    nome = nome_barbeiro_horario_entry.get()
-    nascimento = nascimento_barbeiro_entry.get()
-    cursor.execute('''INSERT INTO Barbeiro (id, nome , nascimento) VALUES (?, ?, ?);''', (id, nome, nascimento))
-    conn.commit()
-    display_barbeiro()
+def dados_barbeiros(): #função para pegar os dados do barbeiros
+    id = id_barbeiro_agenda_entry.get()
+    nome = nome_barbeiro_entry.get()
+    aniversario = aniversario_barbeiro_entry.get()
+    try:
+        cursor.execute('''INSERT INTO Barbeiro (id, nome , aniversario) VALUES (?, ?, ?);''', (id, nome, aniversario))
+        conn.commit()
+        display_barbeiro()
+    except conn.DatabaseError as err:
+        messagebox.showerror('Erro', f'Erro ao inserir barbeiro: {err}')
 
-def display_barbeiro():
+def display_barbeiro(): #função para mostrar os dados já incluso na tabela barbeiros
     cursor.execute (''' SELECT * FROM Barbeiro''')
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-def dados_agenda():
-    id = id_barbeiro_entry.get()
-    nome = nome_barbeiro_horario_entry.get()
+def dados_agenda(): #função para pegar os dados do agendamento
+    id = id_barbeiro_agenda_entry.get()
+    nome = nome_cliente_agenda_entry.get()
     data = data_entry.get()
     hora = horario_entry.get()
-    cursor.execute('''INSERT INTO Agenda (id, nome, data, hora) VALUES (?, ?, ?, ?);''' ,(id, nome, data, hora))
-    conn.commit()
-    display_agenda()
+    
+    try:
+        id=int(id)
+        cursor.execute('''INSERT INTO Agenda (id, nome, data, hora) VALUES (?, ?, ?, ?);''' ,(id, nome, data, hora))
+        conn.commit()
+        display_agenda()
+    except conn.DatabaseError as err:
+        messagebox.showerror('Erro', f'Erro ao agendar horário: {err}')
 
-def display_agenda():
+def display_agenda(): #função para mostrar os dados já inclusos na tabela agenda
     cursor.execute ('''SELECT * FROM Agenda''')
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-root= tk.Tk()
-root.title('Coloque os Dados')
+root= tk.Tk() #interface grafica
+root.title('Plataforma de dados e agendamento')
 
-cpf_label = tk.Label(root, text='CPF: (ex.:"12345678900") ')
+cpf_label = tk.Label(root, text='CPF do Cliente: (somente números!) ')
 cpf_label.grid (row=0, column=0)
 cpf_entry = tk.Entry(root)
 cpf_entry.grid (row=0, column=1)
 
 nome_cliente_label = tk.Label(root, text='Nome Cliente: ')
 nome_cliente_label.grid(row=1, column=0)
-nome_cliente_entry = tk.Entry(root)
-nome_cliente_entry.grid(row=1, column=1)
+nome_cliente_agenda_entry = tk.Entry(root)
+nome_cliente_agenda_entry.grid(row=1, column=1)
 
-nascimento_label = tk.Label(root, text='Nascimento: (ex.:"AAAA-MM-DD/2024-12-31") ')
+nascimento_label = tk.Label(root, text='Nascimento: (ex.: "AAAA-MM-DD/2024-12-31") ')
 nascimento_label.grid(row=2, column=0)
 nascimento_entry = tk.Entry(root)
 nascimento_entry.grid(row=2, column=1)
@@ -68,25 +80,25 @@ contato_label.grid(row=3, column=0)
 contato_entry = tk.Entry(root)
 contato_entry.grid(row=3, column=1)
 
-insert_cliente_button = tk.Button(root, text='Inserir Cliente', command=dados_clientes)
+insert_cliente_button = tk.Button(root, text='Cadastrar Cliente', command=dados_clientes)
 insert_cliente_button.grid(row=4, columnspan=2)
 
 id_barbeiro_label = tk.Label(root, text='ID Barbeiro:')
 id_barbeiro_label.grid(row=5, column=0)
-id_barbeiro_entry = tk.Entry(root)
-id_barbeiro_entry.grid(row=5, column=1)
+id_barbeiro_agenda_entry = tk.Entry(root)
+id_barbeiro_agenda_entry.grid(row=5, column=1)
 
 nome_barbeiro_label = tk.Label(root, text='Nome Barbeiro:')
 nome_barbeiro_label.grid(row=6, column=0)
 nome_barbeiro_entry = tk.Entry(root)
 nome_barbeiro_entry.grid(row=6, column=1)
 
-nascimento_barbeiro_label = tk.Label(root, text='Nascimento: (ex.:"AAAA-MM-DD/2024-12-31") ')
-nascimento_barbeiro_label.grid(row=7,column=0)
-nascimento_barbeiro_entry = tk.Entry(root)
-nascimento_barbeiro_entry.grid(row=7, column=1)
+aniversario_barbeiro_label = tk.Label(root, text='Nascimento: (ex.:"AAAA-MM-DD/2024-12-31") ')
+aniversario_barbeiro_label.grid(row=7,column=0)
+aniversario_barbeiro_entry = tk.Entry(root)
+aniversario_barbeiro_entry.grid(row=7, column=1)
 
-insert_barbeiro_button = tk.Button(root, text='Inserir Barbeiro', command=dados_barbeiros)
+insert_barbeiro_button = tk.Button(root, text='Cadastrar Barbeiro', command=dados_barbeiros)
 insert_barbeiro_button.grid(row=8, columnspan=2)
 
 id_barbeiro_label = tk.Label(root, text='ID Barbeiro:')
@@ -94,10 +106,10 @@ id_barbeiro_label.grid(row=9, column=0)
 id_barbeiro_entry = tk.Entry(root)
 id_barbeiro_entry.grid(row=9, column=1)
 
-nome_barbeiro_horario_label = tk.Label(root, text='Nome Barbeiro:')
-nome_barbeiro_horario_label.grid(row=10, column=0)
-nome_barbeiro_horario_entry = tk.Entry(root)
-nome_barbeiro_horario_entry.grid(row=10, column=1)
+nome_cliente_label = tk.Label(root, text='Nome do Cliente:')
+nome_cliente_label.grid(row=10, column=0)
+nome_cliente_entry = tk.Entry(root)
+nome_cliente_entry.grid(row=10, column=1)
 
 data_label = tk.Label(root, text='Data: (ex.:"AAAA-MM-DD/2024-12-31") ')
 data_label.grid(row=11, column=0)
@@ -109,16 +121,16 @@ horario_label.grid(row=12, column=0)
 horario_entry = tk.Entry(root)
 horario_entry.grid(row=12, column=1)
 
-insert_horario_button = tk.Button(root, text='Inserir Horário', command=dados_agenda)
+insert_horario_button = tk.Button(root, text='Agendar Horário', command=dados_agenda)
 insert_horario_button.grid(row=13, columnspan=2)
-
+#conecta ao meu BD e cria um cursor
 conn = conn.connect('./Barber.db')
 cursor = conn.cursor()
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS Cliente (cpf INTEGER PRIMARY KEY, nome TEXT NOT NULL, nascimento DATE NOT NULL, contato INTEGER NOT NULL)''')
-cursor.execute('''CREATE TABLE IF NOT EXISTS Barbeiro (id INTEGER PRIMARY KEY, nome TEXT NOT NULL)''')
-cursor.execute('''CREATE TABLE IF NOT EXISTS Agenda (id INTEGER NOT NULL, nome TEXT NOT NULL, data DATE NOT NULL, hora TEXT NOT NULL, FOREIGN KEY (id) REFERENCES Barbeiro(id), FOREIGN KEY (nome) REFERENCES Barbeiro(nome))''')
-
+#utiliza o cursor para criar as tabelas caso ainda não tenha criado
+cursor.execute('''CREATE TABLE IF NOT EXISTS Cliente (cpf INTEGER NOT NULL, nome TEXT NOT NULL, nascimento DATE NOT NULL, contato INTEGER NOT NULL, PRIMARY KEY(cpf))''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Barbeiro (id INTEGER NOT NULL, nome TEXT NOT NULL, aniversario DATE NOT NULL, PRIMARY KEY (id))''')
+cursor.execute('''CREATE TABLE IF NOT EXISTS Agenda (id INTEGER  NOT NULL, nome TEXT NOT NULL, data DATE NOT NULL, hora TEXT NOT NULL, FOREIGN KEY (id) REFERENCES Barbeiro(id), FOREIGN KEY (nome) REFERENCES Cliente(nome))''')
+#salva os dados inseridos
 conn.commit()
 
-root.mainloop()
+root.mainloop() 
